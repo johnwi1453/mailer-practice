3c1c9814e9e32b1b04ce9c6668362fe3a405f586
John Williams <Johnwi1453@gmail.com>
2015-09-30 20:04:59 -0700

    Finish user signup feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '@user'
+    else
+      redirect_to 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
